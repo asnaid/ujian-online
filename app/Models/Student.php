6d2b04846e76a6b93e7ffb7b1040a840ad7330f4
Muhammad Haris Rusyana <mrusyana@gmail.com>
2022-09-13 10:00:40 +0700

    sudah bisa import siswa

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -9,14 +9,24 @@ class Student extends Model
 {
     use HasFactory;
 
+    /**
+     * fillable
+     *
+     * @var array
+     */
     protected $fillable = [
         'classroom_id',
         'nisn',
         'name',
         'password',
         'gender'
-    ];
-    
+    ];  
+
+    /**
+     * classroom
+     *
+     * @return void
+     */
     public function classroom()
     {
         return $this->belongsTo(Classroom::class);
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,20 @@ Route::prefix('admin')->group(function() {
 
         //route dashboard
         Route::get('/dashboard', App\Http\Controllers\Admin\DashboardController::class)->name('admin.dashboard');
+    
         //route resource lessons    
         Route::resource('/lessons', \App\Http\Controllers\Admin\LessonController::class, ['as' => 'admin']);
+
+        //route resource classrooms    
+        Route::resource('/classrooms', \App\Http\Controllers\Admin\ClassroomController::class, ['as' => 'admin']);
+        
+        //route student import
+        Route::get('/students/import', [\App\Http\Controllers\Admin\StudentController::class, 'import'])->name('admin.students.import');
+
+        //route student store import
+        Route::post('/students/import', [\App\Http\Controllers\Admin\StudentController::class, 'storeImport'])->name('admin.students.storeImport');
+
+        //route resource students    
+        Route::resource('/students', \App\Http\Controllers\Admin\StudentController::class, ['as' => 'admin']);
     });
 });
